a5294e9b5ec4d523deba3e87b002a9a52977f5a8
Alexander <alex.vostrugo@gmail.com>
2017-03-08 03:31:42 +0200

    docker compose

--- models/task.js
+++ models/task.js
@@ -6,8 +6,6 @@ module.exports = function(sequelize, DataTypes) {
     }, {
         classMethods: {
             associate: function(models) {
-                // Using additional options like CASCADE etc for demonstration
-                // Can also simply do Task.belongsTo(models.User);
                 Task.belongsTo(models.User, {
                     onDelete: "CASCADE",
                     foreignKey: {
